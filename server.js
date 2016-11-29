e266c1e6e399430bb9e7dcb9509144bcdf8e6677
Michele Ferretti <mic.ferretti@gmail.com>
2016-11-29 19:40:02 +0100

    commenting out ws stuff on both client/node. about to use node-osc to input directly from wekinator2client

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var osc = require("osc"),
     http = require("http"),
-    WebSocket = require("ws"),
+    // WebSocket = require("ws"),
     express = require('express'),
     app = express(),
     path = require("path");
@@ -16,26 +16,26 @@ app.get('/', function(req, res){
   res.render('index.html');
 });
 
-// Listen for Web Socket requests.
-var wss = new WebSocket.Server({
-    server: server
-});
-
-// Listen for Web Socket connections.
-wss.on("connection", function (socket) {
-
-  console.log("connection established!");
-
-    var socketPort = new osc.WebSocketPort({
-        socket: socket
-    });
-
-    socketPort.on("message", function (oscMsg) {
-        console.log("An OSC Message was received!", oscMsg);
-    });
-
-    socketPort.send({
-        address: "/carrier/frequency",
-        args: "sadasdasdas"
-    });
-});
+// // Listen for Web Socket requests.
+// var wss = new WebSocket.Server({
+//     server: server
+// });
+//
+// // Listen for Web Socket connections.
+// wss.on("connection", function (socket) {
+//
+//   console.log("connection established!");
+//
+//     var socketPort = new osc.WebSocketPort({
+//         socket: socket
+//     });
+//
+//     socketPort.on("message", function (oscMsg) {
+//         console.log("An OSC Message was received!", oscMsg);
+//     });
+//
+//     socketPort.send({
+//         address: "/carrier/frequency",
+//         args: "sadasdasdas"
+//     });
+// });
